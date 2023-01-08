b55f2d470cf7f2190449861db72026bf66afa29f
Danillo Augusto <githubdanillo_o@outlook.com>
2023-01-08 19:36:35 -0300

    jeito mais facil de transformar uma lista em uma string

---
+++ list-to-string.py
@@ -0,0 +1,7 @@
+# jeito mais facil de transformar uma lista em uma string concatenada.
+my_list = [')', 'N', 'g', 'X', '5', '8', '6', '+', '9']
+
+string = ""
+for char in my_list:
+    string += char
+print(string)
