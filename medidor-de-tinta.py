363138b03daf34cb92bd275270790d32790e80a3
Danillo Augusto <githubdanillo_o@outlook.com>
2023-01-11 21:33:46 -0300

    calcula qts lata de tinta

---
+++ medidor-de-tinta.py
@@ -0,0 +1,21 @@
+#Write your code below this line 👇
+import math
+def paint_calc(height, width, cover):
+    num_can = (height * width) / cover
+    #print(num_can)
+    num_can = math.ceil(num_can)
+    print(f"You'll need {num_can} cans of paint.")
+
+
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
