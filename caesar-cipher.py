faa303f7656935b0365af6a9479799e00c19b23a
Danillo Augusto <githubdanillo_o@outlook.com>
2023-01-12 22:56:46 -0300

    done

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,33 +1,44 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 
-#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 plain_text = list(text)
 num_list = []
 num_list_soma = []
-texto = ""
-#transforma palavra em índice.
-def word_index():
-    
+texto_encripitado = ""
+if direction == "encode":    
+    #transforma palavra em índice.
     for i in plain_text:
         num_list.append(alphabet.index(i))
 
-#soma o número de troca ao índice.
-def shift():
-    
+    #soma o número de troca ao índice.
     for soma in num_list:
         soma += shift
+        if soma > len(alphabet):
+            soma = (soma - len(alphabet))       
         num_list_soma.append(soma)
 
-#transforma índice em palavra.
-def index_word():
-    
+    #transforma índice em palavra.
     for x in num_list_soma:
-        texto += alphabet[x]
+        texto_encripitado += alphabet[x]
 
+if direction == "decode":
+    #transforma palavra em índice.
+    for i in plain_text:
+        num_list.append(alphabet.index(i))
 
+    #soma o número de troca ao índice.
+    for subtracao in num_list:
+        subtracao -= shift
+        #if subtracao < len(alphabet):
+        #    subtracao = (subtracao + len(alphabet))       
+        num_list_soma.append(subtracao)
 
+    #transforma índice em palavra.
+    for x in num_list_soma:
+        texto_encripitado += alphabet[x]    
 
-print(plain_text, num_list, texto, num_list_soma)
+print(plain_text, num_list, texto_encripitado, num_list_soma)
+print(f"The encoded text is {texto_encripitado}")
