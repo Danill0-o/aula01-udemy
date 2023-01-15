b4dff3756a3d7545cc9c0e8f5cd6fb3510a7a834
Danillo Augusto <githubdanillo_o@outlook.com>
2023-01-14 22:56:56 -0300

    Consertado alguns erros

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,35 +1,35 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+def ask():    
+    print("Type it correctly!")
+
+direction = ""
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction == "encode" and direction == "decode":
+        break
+    ask()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+shift = shift % 26
 
-plain_text = list(text)
-num_list = []
 texto_encripitado = ""
 if direction == "encode":    
-    #transforma palavra em índice.
-    for i in plain_text:
-        num_list.append(alphabet.index(i))
-
-    #soma o número de troca ao índice e converte o novo nº na letra correspondente.
-    for soma in num_list:
-        soma += shift
-        if soma >= len(alphabet):
-            soma = soma - len(alphabet)       
+    for i in text:
+        num_list = alphabet.index(i)
+        soma = shift + num_list
+        soma = soma % 26
         texto_encripitado += alphabet[soma]
 
 if direction == "decode":
-    #transforma palavra em índice.
-    for i in plain_text:
-        num_list.append(alphabet.index(i))
-
-    #soma o número de troca ao índice e converte o novo nº na letra correspondente.
-    for subtracao in num_list:
-        subtracao -= shift
+    shift *= -1
+    for i in text:
+        num_list = alphabet.index(i)
+        subtracao = shift + num_list
         texto_encripitado += alphabet[subtracao]
 
-print(plain_text, num_list, texto_encripitado)
+    
+print(text, num_list, texto_encripitado)
 print(f"The encoded text is {texto_encripitado}")
 
 #Solução da Angela:
