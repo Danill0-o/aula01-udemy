1d1a6603a58e9101073262fff5eab604b60e5cf3
Danillo Augusto <githubdanillo_o@outlook.com>
2023-01-16 21:59:48 -0300

    projeto auction house

--- auction-house/auction-house.py
+++ auction-house/auction-house.py
@@ -3,8 +3,6 @@ import os
 
 print(logo)
 
-
-
 #gera um contador fora do arquivo.
 def add_counter():
     if not os.path.isfile('counter.txt'):
@@ -22,15 +20,27 @@ def auction_list(name_inside, bid_inside):
     players["name"] = name_inside
     players["bid"] = bid_inside
     jogadores.append(players)
-#idd = add_counter
+
 jogadores = []
 keep_going = 1
 while keep_going == 1:
+    
     name = input("What is your name, good sir?: ")
     bid = int(input("What is your bid: $"))
     auction_list(name_inside = name, bid_inside = bid)
-    print(jogadores)
-    keep_going = int(input("Are there any other bidders?\n 1 - Yes\n 2 - No \n"))
+    #print(jogadores)
+    keep_going = int(input("Are there any other bidders?\n 1 - Yes\n 2 - No \n "))
+    os.system('cls')
+max_bid = 0
+max_bid_index = 0
+max_bid_name = ""
+for i in range(len(jogadores)):
+    if jogadores[i]["bid"] > max_bid:
+        max_bid = jogadores[i]["bid"]
+        max_bid_index = i
+        max_bid_name = jogadores[i]["name"]
+print(logo)
+print(f"The winner is: {max_bid_name}, bid ${max_bid}!")
 # jogadores = [{
 #      "id": idd,
 #      "name": name,
