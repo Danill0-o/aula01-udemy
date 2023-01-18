8f5a32d5d5f5231e02b45d50966722ab61933891
Danillo Augusto <githubdanillo_o@outlook.com>
2023-01-17 22:05:25 -0300

    leap year 2

---
+++ leap-year-2.py
@@ -0,0 +1,32 @@
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        print("Leap year.")
+        leap = True
+      else:
+        print("Not leap year.")
+        leap = False
+    else:
+      print("Leap year.")
+      leap = True
+  else:
+    print("Not leap year.")
+    leap = False
+  return leap
+
+def days_in_month(year, month):
+    leap = is_leap(year)
+    if leap == False:
+        month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        days = month_days[month - 1]  
+    if leap == True:
+        month_days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        days = month_days[month - 1]
+    return days
+
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
