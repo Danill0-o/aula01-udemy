e1b03b83dea03372538a64d3494eb7366e0c2030
Danillo Augusto <githubdanillo_o@outlook.com>
2023-01-18 20:35:03 -0300

    add a keep going question

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ def subtract(n1, n2):
   return n1 - n2
 
 def divide(n1, n2):
-  return n1 / n2
+  return n1 / n2 
 
 def multiply(n1, n2):
   return n1 * n2
@@ -26,8 +26,19 @@ print(f"\n{list_key}\n")
 print(type(list_key))
 operator_symbol = input("Pick one of the operators above: ")
 num2 = int(input("What's the second number?: "))
-
-
 function = operators[operator_symbol]
-answer = function(n1 = num1, n2 = num2)
+answer = function(num1, num2)
 print(f"{num1} {operator_symbol} {num2} = {answer}")
+
+while True:
+    keep = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
+    if keep == 'y':
+        
+        operator_symbol = input("Pick an operator: ")
+        num3 = int(input("What's the next number?: "))
+        function = operators[operator_symbol]
+        answer2 = function(answer, num3)
+        print(f"{answer} {operator_symbol} {num3} = {answer2}")
+        answer = answer2
+    else:
+        break
